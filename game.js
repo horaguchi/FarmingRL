b1ba348497c77225eaf1ce0c57ad0ad96f71f4b6
Koji Horaguchi <koji_horaguchi@yahoo.co.jp>
2014-03-11 01:35:40 +0900

    Add monster move

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 var Game = {
   display: null,
-  x: 1,
-  y: 17,
+  x: -1,
+  y: -1,
   mapData: {},
+  monsters: [],
   init: function() {
     var width = 60, height = 40;
     //ROT.RNG.setSeed(1234);
@@ -23,50 +24,87 @@ var Game = {
       //this.display.draw(x, y, wall ? "#" : ".");
     });
 
+    // initial player position
     do {
       this.x = parseInt(ROT.RNG.getUniform() * width);
       this.y = parseInt(ROT.RNG.getUniform() * height);
     } while (this.mapData[this.x + "," + this.y] != ".");
 
-
+    // initial test monster
+    for (var i = 0; i < 9; ++i) {
+      var tmpX, tmpY;
+      do {
+        tmpX = parseInt(ROT.RNG.getUniform() * width);
+        tmpY = parseInt(ROT.RNG.getUniform() * height);
+      } while (this.mapData[tmpX + "," + tmpY] != ".");
+      this.addMonster({
+        symbol: String(i),
+        x: tmpX,
+        y: tmpY
+      });
+    }
     this.draw();
     document.body.appendChild(this.display.getContainer());
     document.body.addEventListener("keypress", this.onKeyPress.bind(this));
+    window.setInterval(this.monsterAction.bind(this), 500);
+  },
+  addMonster: function (obj) {
+    this.monsters.push(obj);
+    this.mapData[obj.x + "," + obj.y] = obj.symbol;
+  },
+  monsterAction: function () {
+    var display = this.display;
+    var mapData = this.mapData;
+    var passableCallback = function(x, y) {
+      return (mapData[x + "," + y] == ".");
+    };
+    var playerX = this.x, playerY = this.y;
+    var moved = false;
+    this.monsters.forEach(function (obj) {
+      mapData[obj.x + "," + obj.y] = ".";
+      var i = 0;
+      var dijkstra = new ROT.Path.Dijkstra(playerX, playerY, passableCallback);
+      dijkstra.compute(obj.x, obj.y, function(x, y) {
+        if (i++ == 1 && !(x == playerX && y == playerY) ) {
+          obj.x = x;
+          obj.y = y;
+          moved = true;
+        }
+      });
+      mapData[obj.x + "," + obj.y] = obj.symbol;
+    });
+
+    // redraw if moved
+    if (moved) {
+      this.draw();
+    }
   },
   onKeyPress: function (e) {
     var key = String.fromCharCode(e.charCode);
     var x = this.x, y = this.y;
     var mapData = this.mapData;
     if (key == "h" && mapData[(x - 1) + "," + y] == ".") {
-      this.display.drawText(x, y, ".");
       this.x--;
       this.draw();
     } else if (key == "j" && mapData[x + "," + (y + 1)] == ".") {
-      this.display.drawText(x, y, ".");
       this.y++;
       this.draw();
     } else if (key == "k" && mapData[x + "," + (y - 1)] == ".") {
-      this.display.drawText(x, y, ".");
       this.y--;
       this.draw();
     } else if (key == "l" && mapData[(x + 1) + "," + y] == ".") {
-      this.display.drawText(x, y, ".");
       this.x++;
       this.draw();
     } else if (key == "y" && mapData[(x - 1) + "," + (y - 1)] == ".") {
-      this.display.drawText(x, y, ".");
       this.x--; this.y--;
       this.draw();
     } else if (key == "u" && mapData[(x + 1) + "," + (y - 1)] == ".") {
-      this.display.drawText(x, y, ".");
       this.x++; this.y--;
       this.draw();
     } else if (key == "b" && mapData[(x - 1) + "," + (y + 1)] == ".") {
-      this.display.drawText(x, y, ".");
       this.x--; this.y++;
       this.draw();
     } else if (key == "n" && mapData[(x + 1) + "," + (y + 1)] == ".") {
-      this.display.drawText(x, y, ".");
       this.x++; this.y++;
       this.draw();
     }
@@ -79,12 +117,11 @@ var Game = {
       var ch = (r ? mapData[x + "," + y] : "@");
       display.draw(x, y, ch);
     });
-    display.drawText(this.x, this.y, "@");
   },
   getLightPasses: function (mapData) {
     return function (x, y) {
       var key = x+","+y;
-      if (key in mapData) { return (mapData[key] == "."); }
+      if (key in mapData) { return (mapData[key] != "#"); }
       return false;
     };
   }
