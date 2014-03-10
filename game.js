7c3dc97509373825bca79c73d85ad6c33bc1b1db
Koji Horaguchi <koji_horaguchi@yahoo.co.jp>
2014-03-10 23:51:48 +0900

    Added blocking

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var Game = {
   display: null,
- 
+  x: 1,
+  y: 1,
+  mapArray: {},
   init: function() {
     var map = new ROT.Map.Arena(60, 20);
     this.display = new ROT.Display({
@@ -13,31 +15,49 @@ var Game = {
     });
 
     map.create(function(x, y, wall) {
+      this.mapArray[x + "," + y] = wall ? "#" : ".";
       this.display.draw(x, y, wall ? "#" : ".");
     }.bind(this));
+    this.display.drawText(this.x, this.y, "@");
     document.body.appendChild(this.display.getContainer());
-    var x = 5, y = 5;
-    document.body.addEventListener("keypress", function (e) {
-      var key = String.fromCharCode(e.charCode);
+    document.body.addEventListener("keypress", this.onKeyPress.bind(this));
+  },
+  onKeyPress: function (e) {
+    var key = String.fromCharCode(e.charCode);
+    var x = this.x, y = this.y;
+    var mapArray = this.mapArray;
+    if (key == "h" && mapArray[(x - 1) + "," + y] == ".") {
       this.display.drawText(x, y, ".");
-      if (key == "h") {
-        x--;
-      } else if (key == "j") {
-        y++;
-      } else if (key == "k") {
-        y--;
-      } else if (key == "l") {
-        x++;
-      } else if (key == "y") {
-        x--; y--;
-      } else if (key == "u") {
-        x++; y--;
-      } else if (key == "b") {
-        x--; y++;
-      } else if (key == "n") {
-        x++; y++;
-      }
-      this.display.drawText(x, y, "@");
-    }.bind(this));
+      this.x--;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "j" && mapArray[x + "," + (y + 1)] == ".") {
+      this.display.drawText(x, y, ".");
+      this.y++;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "k" && mapArray[x + "," + (y - 1)] == ".") {
+      this.display.drawText(x, y, ".");
+      this.y--;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "l" && mapArray[(x + 1) + "," + y] == ".") {
+      this.display.drawText(x, y, ".");
+      this.x++;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "y" && mapArray[(x - 1) + "," + (y - 1)] == ".") {
+      this.display.drawText(x, y, ".");
+      this.x--; this.y--;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "u" && mapArray[(x + 1) + "," + (y - 1)] == ".") {
+      this.display.drawText(x, y, ".");
+      this.x++; this.y--;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "b" && mapArray[(x - 1) + "," + (y + 1)] == ".") {
+      this.display.drawText(x, y, ".");
+      this.x--; this.y++;
+      this.display.drawText(this.x, this.y, "@");
+    } else if (key == "n" && mapArray[(x + 1) + "," + (y + 1)] == ".") {
+      this.display.drawText(x, y, ".");
+      this.x++; this.y++;
+      this.display.drawText(this.x, this.y, "@");
+    }
   }
 };
